f102790aab89ef72147d78f3be849d58f747069b
majed samyal <majed.samyal@gmail.com>
2020-01-05 12:16:45 +0530

    spiral matrix

---
+++ Algos/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Algos
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var spiralArr = SpiralMatrix.ReturnSpiralMatrix(5);
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.WriteLine(spiralArr[i, j] +"\n");
+                }
+
+                Console.WriteLine(Environment.NewLine);
+            }
+            Console.ReadLine();
+        }
+
+        
+    }
+}
